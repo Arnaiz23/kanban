91ece9954a23cd7c3d4d23114ed0389723792787
Arnaiz23 <aaf110501@gmail.com>
2024-04-03 21:27:55 +0200

    feat ✨: Change the seed data

--- db/seed.ts
+++ db/seed.ts
@@ -3,6 +3,6 @@ import { db, Project } from 'astro:db';
 // https://astro.build/db/seed
 export default async function seed() {
   await db.insert(Project).values({
-    name: "Kanban", slug: "Kanban"
+    name: "Kanban", slug: "kanban"
   })
 }
