f49ba9f99c28ca4391ff2d5624eaab4246a4c941
Arnaiz23 <aaf110501@gmail.com>
2024-04-13 21:55:14 +0200

    feat ✨: Create the project card and extract the exact information that I need of the project, columns and tasks

--- db/seed.ts
+++ db/seed.ts
@@ -1,8 +1,35 @@
-import { db, Project } from 'astro:db';
+import { Column, db, Project, Task } from "astro:db"
 
 // https://astro.build/db/seed
 export default async function seed() {
   await db.insert(Project).values({
-    name: "Kanban", slug: "kanban"
+    name: "Kanban",
+    slug: "kanban",
   })
+
+  await db.insert(Column).values([
+    {
+      name: "ToDo",
+      project_id: 1,
+    },
+    {
+      name: "Doing",
+      project_id: 1,
+    },
+    {
+      name: "Done",
+      project_id: 1,
+    },
+  ])
+
+  await db.insert(Task).values([
+    {
+      name: "TEst",
+      column_id: 1,
+    },
+    {
+      name: "Testing2",
+      column_id: 3,
+    },
+  ])
 }
